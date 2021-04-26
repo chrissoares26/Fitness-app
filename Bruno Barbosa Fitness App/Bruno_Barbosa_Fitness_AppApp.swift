4531b58500c6b8e61f2cf9f6451e2b471e390d6a
chrissoares26 <sou20001@byui.edu>
2021-04-26 14:00:09 -0600

    Initial Commit

---
+++ Bruno Barbosa Fitness App/Bruno_Barbosa_Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bruno_Barbosa_Fitness_AppApp.swift
+//  Bruno Barbosa Fitness App
+//
+//  Created by Christopher Soares on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Bruno_Barbosa_Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
