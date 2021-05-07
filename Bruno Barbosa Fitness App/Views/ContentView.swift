e93180408ec9a0c038b19acc716785d236658c30
chrissoares26 <sou20001@byui.edu>
2021-05-07 17:01:19 -0600

    Created stretching UI main page

--- Bruno Barbosa Fitness App/Views/ContentView.swift
+++ Bruno Barbosa Fitness App/Views/ContentView.swift
@@ -9,48 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Image("background")
-                .ignoresSafeArea()
-            
-            VStack{
-                Image("logo")
-                VStack {
-                    
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    HStack{
-                        
-                        Image(systemName: "heart.text.square").foregroundColor(Color.white).padding()
-                        Text("IMC")
-                            .font(.title)
-                            .foregroundColor(Color.white).padding()
-                            
-
+        NavigationView {
+            VStack(alignment: .leading){
+                ZStack {
+                    Image("stretching-6")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .frame(height: 300)
+                    VStack{
+                        Spacer()
+                        Text("Full Body Stretching")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("For Beginners")
+                            .fontWeight(.regular)
+                            .foregroundColor(.white)
                     }
-                }).background(Color.yellow).cornerRadius(15)
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        HStack{
-                            Image(systemName: "heart.text.square").foregroundColor(Color.white).padding()
-                            Text("IMC")
-                                .font(.title)
-                                .foregroundColor(Color.white).padding()
-                            
-                        }
-                    }).background(Color.yellow).cornerRadius(15)
-                   
+                    .padding()
+                    .frame(width: 380)
+                    .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+                }
+                .frame(width: 380, height: 300)
+                .cornerRadius(20)
+                .clipped()
+                .shadow(radius: 8)
+                //.padding(.top, 20)
+                .padding()
                 
+                
+                Text("Weekly Plan")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding()
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 30) {
+                        
+                        ForEach(0 ..< 5) { item in
+                            ZStack {
+                                Image("stretching-1")
+                                    .resizable()
+                                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                    .frame(height: 220)
+                                VStack{
+                                    Spacer()
+                                    Text("Monday")
+                                        .font(.title)
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.white)
+                                }
+                                .padding()
+                                .frame(width: 150)
+                                .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+                            }
+                            .frame(width: 150, height: 220)
+                            .clipped()
+                            .cornerRadius(20)
+                            .shadow(radius: 8)
+                        }
+                    
+                    
+                    }
+                    .padding()
+                   
                 }
-                }
+                .offset(x:0, y: -20)
+                
+                Spacer()
                 
             }
-            
-            
+            .navigationBarTitle("Home Stretching")
         }
         
-            
-            
-
+    }
+    
 }
     
 
